ed674f332f2d0229b18e654b8d9663f462e51a26
Laddge <dev.laddge@gmail.com>
2023-05-20 06:51:45 +0000

    fix alias settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,3 @@
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
 
 import { defineConfig } from 'astro/config'
 
@@ -8,11 +6,4 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  vite: {
-    resolve: {
-      alias: {
-        '@/': `${path.resolve(dirname(fileURLToPath(import.meta.url)), 'src')}/`,
-      },
-    },
-  },
 })

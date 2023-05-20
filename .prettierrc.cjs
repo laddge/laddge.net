64d623534be8dd43feb1053842b4a3e4625c98e1
Laddge <dev.laddge@gmail.com>
2023-05-20 03:27:56 +0000

    add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};

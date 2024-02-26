436d50732521b22049df897e50611c164285f49d
Laddge <dev.laddge@gmail.com>
2024-02-26 11:36:27 +0900

    feat: :sparkles: 画像読み込みの動作を変更

--- src/components/ArticleList.svelte
+++ src/components/ArticleList.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import 'lazysizes'
   import { format } from 'date-fns'
 
   import type { Article } from '@/lib/getArticles'
@@ -14,16 +13,12 @@
         class="flex-none w-32 aspect-[1200/630] rounded-xl bg-base-200 bg-no-repeat bg-center bg-[length:2rem_2rem] overflow-hidden"
         style="background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9Im5vbmUiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZT0iY3VycmVudENvbG9yIj48cGF0aCBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGQ9Ik0xOS41IDE0LjI1di0yLjYyNWEzLjM3NSAzLjM3NSAwIDAwLTMuMzc1LTMuMzc1aC0xLjVBMS4xMjUgMS4xMjUgMCAwMTEzLjUgNy4xMjV2LTEuNWEzLjM3NSAzLjM3NSAwIDAwLTMuMzc1LTMuMzc1SDguMjVtMCAxMi43NWg3LjVtLTcuNSAzSDEyTTEwLjUgMi4yNUg1LjYyNWMtLjYyMSAwLTEuMTI1LjUwNC0xLjEyNSAxLjEyNXYxNy4yNWMwIC42MjEuNTA0IDEuMTI1IDEuMTI1IDEuMTI1aDEyLjc1Yy42MjEgMCAxLjEyNS0uNTA0IDEuMTI1LTEuMTI1VjExLjI1YTkgOSAwIDAwLTktOXoiIC8+PC9zdmc+Cg==);"
       >
-        <div
-          class="article-image w-full h-full bg-no-repeat bg-left bg-cover opacity-0 transition-opacity duration-100 lazyload"
-          style:--bgImg={`url(${article.image})`}
+        <img
+          src={article.image}
+          alt={article.slug}
+          class="w-full h-full object-contain"
+          loading="lazy"
         />
-        <style>
-          .article-image.lazyloaded {
-            background-image: var(--bgImg);
-            opacity: 1;
-          }
-        </style>
       </div>
       <div class="min-w-0 grow">
         <a

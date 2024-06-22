d14ebbee072b5c9b24a88797e3b2d21098e20417
Laddge <dev.laddge@gmail.com>
2024-06-22 18:50:04 +0900

    feat: :sparkles: タグの検索UIを変更

--- src/components/SearchArticles.svelte
+++ src/components/SearchArticles.svelte
@@ -53,43 +53,63 @@
   }
 </script>
 
-<div class="search-block p-6 mb-8 rounded-box border">
+<div class="search-block p-6 mb-8 rounded-box border grid gap-4">
   <input
     type="text"
     placeholder="Search"
     bind:value={query}
     class="input input-bordered input-primary w-full"
   />
-  <div class="pt-3">
-    <h3 class="text-xl font-bold mb-1"># tags</h3>
-    <div class="overflow-auto h-44 bg-base-100 border rounded-lg px-2">
+  {#if tags.length}
+    <div class="flex flex-wrap">
       {#each Array.from(new Set([].concat(...allArticles.map((article) => article.tags))))
         .sort()
         .filter((tag) => tags.indexOf(tag) != -1) as tag}
-        <label class="block flex items-center gap-2 my-2">
-          <input
-            type="checkbox"
-            bind:group={tags}
-            value={tag}
-            class="checkbox checkbox-sm checkbox-primary"
-          />
-          {tag}
-        </label>
-      {/each}
-      {#each Array.from(new Set([].concat(...allArticles.map((article) => article.tags))))
-        .sort()
-        .filter((tag) => tags.indexOf(tag) == -1) as tag}
-        <label class="block flex items-center gap-2 my-2">
-          <input
-            type="checkbox"
-            bind:group={tags}
-            value={tag}
-            class="checkbox checkbox-sm checkbox-primary"
-          />
-          {tag}
-        </label>
+        <div class="bg-base-200 px-2 py-1 rounded flex items-center mr-2 my-1 text-sm">
+          #{tag}
+          <label class="btn btn-sm btn-ghost p-0 h-auto min-h-0 ml-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-4 w-4"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+            <input type="checkbox" bind:group={tags} value={tag} class="hidden" />
+          </label>
+        </div>
       {/each}
     </div>
+  {/if}
+  <div class="dropdown">
+    <button class="btn btn-sm btn-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2.5"
+        stroke="currentColor"
+        class="h-4 w-4"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+      #tag
+    </button>
+    <ul
+      class="dropdown-content z-[1] menu block sm:flex p-2 border shadow-lg bg-base-100 rounded-box overflow-auto h-[calc(11rem+1px)] w-full mt-2"
+    >
+      {#each Array.from(new Set([].concat(...allArticles.map((article) => article.tags)))).sort() as tag}
+        <li class="p-1">
+          <label class={`px-2 py-1 ${tags.indexOf(tag) !== -1 ? 'bg-base-200' : ''}`}>
+            <input type="checkbox" bind:group={tags} value={tag} class="hidden" />
+            #{tag}
+          </label>
+        </li>
+      {/each}
+    </ul>
   </div>
 </div>
 <div class="tabs tabs-bordered mt-2 mb-4">

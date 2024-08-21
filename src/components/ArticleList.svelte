23d8c6f2833fa0f66315690e53192e0152ac81ed
Laddge <dev.laddge@gmail.com>
2024-08-22 08:23:41 +0900

    fix: 記事リストの画像のプレースホルダーのサイズを修正

--- src/components/ArticleList.svelte
+++ src/components/ArticleList.svelte
@@ -12,7 +12,7 @@
       <a
         href={article.href}
         target={article.articleType ? '_blank' : ''}
-        class="btn btn-ghost h-auto border-none p-0 flex-none sm:w-32 sm:max-w-[30vw] aspect-[1200/630] rounded-xl bg-base-200 bg-no-repeat bg-center bg-[length:2rem_2rem] overflow-hidden"
+        class="btn btn-ghost w-full h-auto border-none p-0 flex-none sm:w-32 sm:max-w-[30vw] aspect-[1200/630] rounded-xl bg-base-200 bg-no-repeat bg-center bg-[length:2rem_2rem] overflow-hidden"
         style="background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9Im5vbmUiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZT0iY3VycmVudENvbG9yIj48cGF0aCBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGQ9Ik0xOS41IDE0LjI1di0yLjYyNWEzLjM3NSAzLjM3NSAwIDAwLTMuMzc1LTMuMzc1aC0xLjVBMS4xMjUgMS4xMjUgMCAwMTEzLjUgNy4xMjV2LTEuNWEzLjM3NSAzLjM3NSAwIDAwLTMuMzc1LTMuMzc1SDguMjVtMCAxMi43NWg3LjVtLTcuNSAzSDEyTTEwLjUgMi4yNUg1LjYyNWMtLjYyMSAwLTEuMTI1LjUwNC0xLjEyNSAxLjEyNXYxNy4yNWMwIC42MjEuNTA0IDEuMTI1IDEuMTI1IDEuMTI1aDEyLjc1Yy42MjEgMCAxLjEyNS0uNTA0IDEuMTI1LTEuMTI1VjExLjI1YTkgOSAwIDAwLTktOXoiIC8+PC9zdmc+Cg==);"
       >
         <img

51f505ed8ee966a064d2e062bb75643e0c12207e
Laddge <dev.laddge@gmail.com>
2026-01-12 00:28:44 +0900

    install daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     allowedHosts: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });

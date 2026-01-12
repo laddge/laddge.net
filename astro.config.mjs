f1b490b8ffe7f1df1a4a66dd628592891283b27a
Laddge <dev.laddge@gmail.com>
2026-01-12 17:35:08 +0900

    configure metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import cname from 'vite-plugin-cname'
+
+const CNAME = 'laddge.net'
 
 // https://astro.build/config
 export default defineConfig({
+  site: `https://${CNAME}`,
   server: {
     allowedHosts: true,
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), cname(CNAME)],
   },
 });

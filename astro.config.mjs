e42c3b5796770e074cdf8367e3fe1199007c3ee4
Laddge <dev.laddge@gmail.com>
2026-01-13 15:57:45 +0900

    install postcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 
 // https://astro.build/config
@@ -11,9 +10,6 @@ export default defineConfig({
   },
   build: {
     assets: 'assets',
-  },
-  vite: {
-    plugins: [tailwindcss()],
   },
   integrations: [icon()],
 })

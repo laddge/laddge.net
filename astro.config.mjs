7a1814469a30db530ab9726d032d191ce1ff2d0b
Laddge <dev.laddge@gmail.com>
2026-01-13 17:02:35 +0900

    scopedStyleStrategy: "where"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   site: `https://laddge.net`,
+  scopedStyleStrategy: 'where',
   server: {
     allowedHosts: true,
   },

bdf09268517f475d53d7c298978f87f2d39d74a4
Laddge <dev.laddge@gmail.com>
2026-01-20 19:52:02 +0900

    validate in script

--- scripts/new.ts
+++ scripts/new.ts
@@ -6,27 +6,36 @@ import handlebars from 'handlebars'
 
 const date = new Date()
 
+const { type } = await prompts([{
+  type: 'select',
+  name: 'type',
+  message: 'Type?',
+  choices: [
+    { title: 'Blog', value: 'blog' },
+    { title: 'Works', value: 'works' },
+  ],
+}])
+
+const basePath = path.join(import.meta.dirname, `../src/content/${type}`)
+
+const { slug } = await prompts([{
+  type: 'text',
+  name: 'slug',
+  message: 'Slug?',
+  initial: format(date, 'yyyyMMdd'),
+  validate: async slug => {
+    if (!/^[a-z0-9]+(-[a-z0-9]+)*$/.test(slug)) return false
+    if (await Bun.file(path.join(basePath, `${slug}/index.mdx`)).exists()) return false
+    return true
+  },
+}])
+
 const questions: PromptObject[] = [
-  {
-    type: 'select',
-    name: 'type',
-    message: 'Type?',
-    choices: [
-      { title: 'Blog', value: 'blog' },
-      { title: 'Works', value: 'works' },
-    ],
-  },
-  {
-    type: 'text',
-    name: 'slug',
-    message: 'Slug?',
-    initial: format(date, 'yyyyMMdd'),
-  },
   {
     type: 'text',
     name: 'title',
     message: 'Title?',
-    validate: value => value ? true : false,
+    validate: title => title ? true : false,
   },
   {
     type: 'text',
@@ -49,11 +58,9 @@ const questions: PromptObject[] = [
 
 const res = await prompts(questions)
 
-const basePath = path.join(import.meta.dirname, `../src/content/${res.type}`)
-
 const template = handlebars.compile(await Bun.file(path.join(basePath, '_template/index.mdx')).text())
 
-const outPath = path.join(basePath, `${res.slug}/index.mdx`)
+const outPath = path.join(basePath, `${slug}/index.mdx`)
 
 await Bun.write(outPath, template({
   ...res,
